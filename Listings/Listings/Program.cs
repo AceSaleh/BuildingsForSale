c87cf662cfe4ef74d782ce3a99bb9d1e17f7a23a
Adam Saleh <Adam.Saleh@jato.com>
2020-02-17 10:29:43 -0500

    Added new message

---
+++ Listings/Listings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Listings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listings for all buildings for sale");
+        }
+    }
+}
